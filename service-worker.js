4a2d654ff0d41aaac6ee36ab37ffbc034f16e681
dphnll <impressia.nn@gmail.com>
2026-08-01 13:53:54 +0300

    Keep Travel Idea destination actions sticky

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "backpacker-pwa-v79";
+const CACHE_NAME = "backpacker-pwa-v80";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css",
+  "./styles.css?v=travel-idea-sticky-actions-20260801",
   "./financial-core.js",
   "./trip-date-core.js",
   "./trip-draft-quantity-core.js",
